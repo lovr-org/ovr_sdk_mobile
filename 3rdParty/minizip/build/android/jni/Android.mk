f88e937390700ce7e8c0c8bc3f15edcbdda4bab4
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-04-09 13:53:49 +0200

    v1.32.0 aka Oculus Mobile SDK 15.0

---
+++ 3rdParty/minizip/build/android/jni/Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)/../../..
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := minizip
+LOCAL_ARM_MODE := arm
+LOCAL_ARM_NEON := true
+
+include $(LOCAL_PATH)/../../cflags.mk
+
+define recursive_wildcard
+  $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call recursive_wildcard, $(e)))
+endef
+
+# Include headers.
+HEADER_LIST := $(filter %.h, $(call recursive_wildcard, $(LOCAL_PATH)))
+LOCAL_HEADERS := $(HEADER_LIST:$(LOCAL_PATH)/%=%)
+
+LOCAL_SRC_FILES := \
+  src/ioapi.c \
+  src/miniunz.c \
+  src/mztools.c \
+  src/unzip.c \
+  src/zip.c
+  
+  	
+LOCAL_CFLAGS	+= -Wno-shadow
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src
+
+LOCAL_EXPORT_LDLIBS += -lz
+
+include $(BUILD_STATIC_LIBRARY)
